3409da3dbe5a8c5a4dfbfd5634f00a2bed6d1baf
wissal salmi <60981790+wissal12345@users.noreply.github.com>
2021-11-15 01:40:46 +0100

    Ajout EJB CustomerManager

---
+++ src/main/java/fr/grin/tpcustomerapplication/session/CustomerManager.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/J2EE/EJB30/StatelessEjbClass.java to edit this template
+ */
+package fr.grin.tpcustomerapplication.session;
+
+import fr.grin.tpcustomerapplication.Customer;
+import javax.ejb.Stateless;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+/**
+ *
+ * @author p
+ */
+@Stateless
+public class CustomerManager {
+ @PersistenceContext(unitName = "customerPU")
+    private EntityManager em;
+    public List<Customer> getAllCustomers() {
+       Query query = em.createNamedQuery("Customer.findAll");
+       return query.getResultList();
+    }
+
+    public Customer update(Customer customer) {
+       return em.merge(customer);
+    }
+
+    public void persist(Customer customer) {
+       em.persist(customer);
+    }
+}
