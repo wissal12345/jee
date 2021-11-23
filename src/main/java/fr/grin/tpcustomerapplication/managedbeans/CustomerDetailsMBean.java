453b74d211a3ec8c5abe6aec2ca7055091e5a7f3
wissal salmi <60981790+wissal12345@users.noreply.github.com>
2021-11-24 00:07:49 +0100

    detail

---
+++ src/main/java/fr/grin/tpcustomerapplication/managedbeans/CustomerDetailsMBean.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fr.grin.tpcustomerapplication.managedbeans;
+
+import fr.grin.tpcustomerapplication.Customer;
+import fr.grin.tpcustomerapplication.session.CustomerManager;
+import java.io.Serializable;
+import javax.ejb.EJB;
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author p
+ */
+@Named
+@ViewScoped
+public class CustomerDetailsMBean implements Serializable {
+  private int idCustomer;
+  private Customer customer;
+
+  @EJB
+  private CustomerManager customerManager;
+
+  public int getIdCustomer() {
+    return idCustomer;
+  }
+
+  public void setIdCustomer(int idCustomer) {
+    this.idCustomer = idCustomer;
+  }
+
+  /**
+   * Retourne les détails du client courant (celui dans l'attribut customer de
+   * cette classe), qu'on appelle une propriété (property)
+   */
+    public Customer getDetails() {
+      return customer;
+    }
+
+  /**
+   * Action handler - met à jour dans la base de données les données du client
+   * contenu dans la variable d'instance customer.
+   * @return la prochaine page à afficher, celle qui affiche la liste des clients.
+   */
+  public String update() {
+    // Modifie la base de données.
+    // Il faut affecter à customer.
+    customer = customerManager.update(customer);
+    return "CustomerList";
+  }
+
+  public void loadCustomer() {
+    this.customer = customerManager.getCustomer(idCustomer);
+  }
+}
--- src/main/java/fr/grin/tpcustomerapplication/session/CustomerManager.java
+++ src/main/java/fr/grin/tpcustomerapplication/session/CustomerManager.java
@@ -32,4 +32,8 @@ public class CustomerManager {
     public void persist(Customer customer) {
        em.persist(customer);
     }
+
+   public Customer getCustomer(int idCustomer) {  
+  return em.find(Customer.class, idCustomer);  
+}
 }
